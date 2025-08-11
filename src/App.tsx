2ea533efddcb9a1b4dd5de3f2ce4f16443fe7cfa
WolWha <leeyoon97@naver.com>
2025-08-11 18:56:53 +0900

    🐛 Bug: setShowMenu 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ export default function App() {
 
   const handleUpload = () => {
     photoRef.current?.click();
-    setShowMenu(false);
   };
 
   const setBGImg = (e: React.ChangeEvent<HTMLInputElement>) => {
