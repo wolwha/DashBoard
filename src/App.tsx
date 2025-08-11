9cbde7a7097f1e0b28e5cfa039c8db0a77fcf333
WolWha <leeyoon97@naver.com>
2025-08-11 15:59:50 +0900

    ✨ Feat: 시간, 날짜 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
 import { CgMenuGridR } from "react-icons/cg";
 
 export default function App() {
+  const [time, setTime] = useState<Date>(new Date());
+  useEffect(() => {
+    const currentTime = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(currentTime);
+  }, []);
   return (
     <>
       <div className="relative justify-center items-center flex w-[100vw] h-[100vh] text-white">
         <div className="flex flex-col font-extrabold items-end">
-          <p className="text-[50px]">시계</p>
-          <p className="text-[25px]">날짜</p>
+          <p className="text-[100px]">
+            {time.getHours().toString().padStart(2, "0")} :{" "}
+            {time.getMinutes().toString().padStart(2, "0")}
+          </p>
+          <p className="text-[30px]">
+            {(time.getMonth() + 1).toString().padStart(2, "0")}월{" "}
+            {time.getDate().toString().padStart(2, "0")}일
+          </p>
         </div>
         <button className="bg-gray-300/20 z-10 absolute right-[20px] top-[20px] size-[40px] flex justify-center items-center text-[50px] rounded-[5px] cursor-pointer shadow-2xl">
           <CgMenuGridR />
